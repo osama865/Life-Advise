baf5f8fef64e7374bd554484af129969ffe8d734
osama ibrahim <osama@Device-F58698.osama>
2021-12-29 13:00:47 +0200

    hmmm service worker

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,7 @@ import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import { App } from "/imports/ui/App";
 import { BrowserRouter as Router } from "react-router-dom";
+import "../imports/startup/client/serviceWorker"
 Meteor.startup(() => {
   render(
     <Router>
