056f8d180dcbfa2f6b1cdb7f352994a363e6ea83
osama ibrahim <osama@Device-F58698.osama>
2021-12-31 17:23:28 +0200

    offline not working good

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import FetchSavedAdvises from "./components/fetch-saved";
 import FetchOneAdvise from "./components/random";
 import NotFound from "./components/404";
 import FetchAllAdvises from "./components/all";
 import Navbar from "./components/navbar";
+import Offline from "./components/offline";
 export const App = () => {
+  const [online, setOnline] = useState(navigator.onLine);
+  window.addEventListener("online", () => {
+    console.log("online");
+    setOnline(true);
+  });
+  window.addEventListener("offline", () => {
+    console.log("offline");
+    setOnline(false);
+  });
+
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route exact path="/saved" element={<FetchSavedAdvises />} />
         <Route exact path="/random" element={<FetchOneAdvise />} />
@@ -16,6 +28,8 @@ export const App = () => {
         <Route exact path="/all" element={<FetchAllAdvises />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+
+      {online === false && <Offline />}
     </>
   );
 };
---
+++ imports/ui/components/offline/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Offline() {
+    alert('You Are Offline now , check your internet connection')
+    return (
+        <div>
+            You are currently Offline , please check your internet connection
+        </div>
+    )
+}
